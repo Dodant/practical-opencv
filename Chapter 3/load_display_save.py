24b5b8bb9708712cf83826e91f897f9a6c58f528
Dodanto <ohho0728@naver.com>
2020-03-09 18:30:25 +0900

    Fin : Chp 3 & Error Fixed

--- Chapter 3/load_display_save.py
+++ Chapter 3/load_display_save.py
@@ -1,17 +1,15 @@
-from __future__ import print_function
 import argparse
 import cv2
 
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--image", required=True, help="Path to the image")
-# args = vars(ap.parse_args())
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
 
-image = cv2.imread("../images/trex.png")
+image = cv2.imread(args["image"])
 print("width: {} pixels".format(image.shape[1]))
 print("height: {} pixels".format(image.shape[0]))
 print("channel: {}".format(image.shape[2]))
 
 cv2.imshow("Image", image)
 cv2.waitKey(0)
-cv2.distroyAllWindows()
 cv2.imwrite("newimage.jpg", image)

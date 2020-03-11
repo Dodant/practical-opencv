ec77d6667c9126e13ecbd2d85dfb8bd3d9c273dc
Dodanto <ohho0728@naver.com>
2020-03-12 00:23:27 +0900

    Chp 6.6 : Color Spaces

---
+++ Chapter 6/colorspaces.py
@@ -0,0 +1,23 @@
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+cv2.waitKey(0)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray", gray)
+cv2.waitKey(0)
+
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+cv2.imshow("HSV", hsv)
+cv2.waitKey(0)
+
+lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+cv2.imshow("L*a*b*", lab)
+cv2.waitKey(0)
